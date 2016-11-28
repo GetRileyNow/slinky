9c1550c501812cc1491d5c21bd7d5ef726a8b2df
Helson Taveras <helson@getrileynow.com>
2016-11-28 11:52:57 -0500

    Sending to slack with specified webhook

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 const program = require('commander');
 const version = require('./package.json').version;
+const CHANNEL = '@helsont';
 
 program
   .version(version)
-  .option('-u, --url <url>', 'Slack webhook url', '')
-  .option('-a, --alias', 'Username to use', 'default')
-  .option('-t, --text', 'Notification text', '')
+  .option('-u, --url <url>', 'Slack webhook url')
+  .option('-t, --text <text>', 'Notification text')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.url) console.log('  - peppers');
-if (program.alias) console.log('  - pineapple');
-if (program.text) console.log('  - bbq');
-console.log('  - %s cheese', program.url);
+if (!program.url) {
+  return console.log('Must specify a url arg');
+}
+
+if (!program.text) {
+  return console.log('Must specify a text arg');
+}
+
+const Slack = require('./lib/slack');
+const slack = new Slack(program.url);
+
+slack
+  .notify(program.channel, program.text)
+  .then(console.log)
+  .catch(console.error);
